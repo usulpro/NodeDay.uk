faf6aa01953397218816e363f077e29777a603ed
Oleg Proskurin <regx@usul.su>
2020-02-21 01:51:02 +0300

    Update template

--- gulp/conference-settings.js
+++ gulp/conference-settings.js
@@ -1,3 +1,6 @@
+const conferenceTitle = 'Node_Day_UK';
+const eventYear = 'Y2020';
+
 const tagColors = {
   NodeJS: {
     tagBG: '#7AB464',
@@ -59,4 +62,6 @@ const speakerAvatar = {
 module.exports = {
   tagColors,
   speakerAvatar,
+  conferenceTitle,
+  eventYear,
 };
